95da1b6867fc869a7541545b121e545729f5c83d
Stanislav Sklyarov <stanislav.sklyarov@gmail.com>
2025-10-04 15:55:44 +0300

    .

--- Tests/FlintTests/AssertExtensions.cs
+++ Tests/FlintTests/AssertExtensions.cs
@@ -5,19 +5,19 @@ namespace FlintTests
 	static class AssertExtensions
 	{
 		#region Interface
-		public static void AssertNotEmpty<T>(this IEnumerable<T> actual)
+		public static void AssertNotEmpty<T>(this IEnumerable<T> col)
 		{
-			if (actual.Any() == false)
+			if (col.Any() == false)
 				Assert.Fail("Collection is empty.");
 		}
 
-		public static void AssertEmpty<T>(this IEnumerable<T> actual)
+		public static void AssertEmpty<T>(this IEnumerable<T> col)
 		{
-			if (actual.Any())
+			if (col.Any())
 				Assert.Fail("Collection is not empty.");
 		}
 
-		public static void AssertSame<T>(this ICollection<T> actual, ICollection<T> expected)
+		public static void AssertSame<T>(this IReadOnlyCollection<T> actual, IReadOnlyCollection<T> expected)
 		{
 			var diffExpected = expected.Except(actual).ToList();
 			var diffActual = actual.Except(expected).ToList();
@@ -32,10 +32,30 @@ namespace FlintTests
 			Assert.Fail(sb.ToString());
 		}
 
-		public static void AssertContains<T>(this ICollection<T> actual, Func<T, bool> predicate)
+		public static void AssertContains<T>(this IReadOnlyCollection<T> col, Func<T, bool> predicate)
 		{
-			if (actual.Any(predicate) == false)
+			if (col.Any(predicate) == false)
 				Assert.Fail("Collection does not contain a required items.");
+		}
+
+		public static void AssertCount<T>(this IReadOnlyCollection<T> col, int count)
+		{
+			if (col.Count != count)
+				Assert.Fail($"Collection is expected to have {count} items, got {col.Count}.");
+		}
+
+		public static void AssertEquals<T>(this T actual, T expected)
+		{
+			if (actual.Equals(expected))
+				return;
+
+			var sb = new StringBuilder();
+			sb.AppendLine("Values are not equal.");
+			sb.AppendLine("=== Expected:");
+			sb.AppendLine(expected.ToString());
+			sb.AppendLine("=== Actual:");
+			sb.AppendLine(actual.ToString());
+			Assert.Fail(sb.ToString());
 		}
 		#endregion
 
