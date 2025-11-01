73045ec698d1653ea289226400f9f997dcb53719
Stanislav Sklyarov <stanislav.sklyarov@gmail.com>
2025-11-01 17:54:03 +0200

    .

--- Sources/FlintWeb/Pages/Index.cshtml.cs
+++ Sources/FlintWeb/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
+using System.Text;
 using FlintWeb.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -42,28 +43,27 @@ namespace FlintWeb.Pages
 			ElapsedMilliseconds = 0;
 			Error = null;
 
-			using (_logger.BeginScope("OnPost"))
+			var traceId = Guid.NewGuid();
+			_logger.LogInformation($"[{traceId}] processing file {FileName}");
+			using (var dllStream = DllFile.OpenReadStream())
+			using (var pdbStream = PdbFile?.OpenReadStream())
 			{
-				_logger.LogInformation($"Processing file {FileName}");
-				using (var dllStream = DllFile.OpenReadStream())
-				using (var pdbStream = PdbFile?.OpenReadStream())
+				var watch = Stopwatch.StartNew();
+				try
 				{
-					var watch = Stopwatch.StartNew();
-					try
-					{
-						Result = _flint.Analyze(dllStream, pdbStream);
-					}
-					catch (Exception ex)
-					{
-						_logger.LogError(ex.ToString());
-						Error = ex.Message;
-					}
-					watch.Stop();
-					ElapsedMilliseconds = watch.ElapsedMilliseconds;
+					Result = _flint.Analyze(dllStream, pdbStream);
 				}
-				if (Error == null)
-					_logger.LogInformation($"Finished in {ElapsedMilliseconds} ms");
+				catch (Exception ex)
+				{
+					_logger.LogError($"[{traceId}] ERROR {ex}");
+					Error = ex.Message;
+				}
+				watch.Stop();
+				ElapsedMilliseconds = watch.ElapsedMilliseconds;
 			}
+			if (Error == null)
+				_logger.LogInformation($"[{traceId}] finished in {ElapsedMilliseconds} ms");
+
 			ShowResult = true;
 			return Page();
 		}
