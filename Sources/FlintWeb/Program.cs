fe12ae0b8dd72774faa7d86a7672a016e94fcdc8
Stanislav Sklyarov <stanislav.sklyarov@gmail.com>
2025-11-18 13:14:26 +0200

    .

--- Sources/FlintWeb/Program.cs
+++ Sources/FlintWeb/Program.cs
@@ -3,23 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplicationInsightsTelemetry();
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddApplicationInsights();
-
 builder.Services.AddRazorPages(options =>
 {
 	options.Conventions.ConfigureFilter(new AutoValidateAntiforgeryTokenAttribute());
 });
 builder.Services.AddScoped<IFlintService, FlintService>();
-
-var storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-var storageContainerName = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONTAINER_NAME");
-if (builder.Environment.IsDevelopment())
-	builder.Services.AddScoped<IStorageService>((_) => new FileSystemStorageService(storageConnectionString, storageContainerName));
-else
-	builder.Services.AddScoped<IStorageService>((_) => new AzureStorageService(storageConnectionString, storageContainerName));
 
 var app = builder.Build();
 
